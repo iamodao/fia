4462d9b79d13ec7e9af4b0c8108f10bbd81c9fa7
Anthony O. Osawere <anthony@osawere.com>
2020-05-30 22:16:25 +0100

    Update config.php

--- content/config.php
+++ content/config.php
@@ -8,9 +8,14 @@ $oInitConfig = array(
 		'domain' => 'fia.go',
 		'email' => 'admin@fia.go',
 		'phone' => '+44 123-456-7890',
-		'version' => 'alpha',
+		'version' => 'evolving',
 	),
-
+	'database' => array (
+		'host' => 'localhost',
+		'name' => 'fia_db',
+		'user' => 'fia',
+		'password' => 'FIA'
+	),
 	'isIn' => 'oDev',
 	'isCond' => 'oLive',
 	'imposeSSL' => 'oYeah',
