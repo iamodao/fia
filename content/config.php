28ad2d458f3572f02128cbf187b9d38182512efe
Anthony O. Osawere <anthony@osawere.com>
2020-05-30 22:15:20 +0100

    Update config.php

--- content/config.php
+++ content/config.php
@@ -1,5 +1,16 @@
 <?php
 $oInitConfig = array(
+	'project' => array(
+		'firm' => 'FIA Framework by OA™',
+		'name' => 'FIA Framework',
+		'brand' => 'FIA™',
+		'baseURL' => 'https://fia.go',
+		'domain' => 'fia.go',
+		'email' => 'admin@fia.go',
+		'phone' => '+44 123-456-7890',
+		'version' => 'alpha',
+	),
+
 	'isIn' => 'oDev',
 	'isCond' => 'oLive',
 	'imposeSSL' => 'oYeah',
