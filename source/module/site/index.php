fb30b51ef061fb65abcb7626cfb764ce4d51b727
Anthony O. Osawere <anthony@osawere.com>
2020-06-02 21:15:32 +0100

    Create index.php

---
+++ source/module/site/index.php
@@ -0,0 +1,3 @@
+<h1>SITE INDEX</h1>
+<p>This is the default controller for <em>website</em></p>
+<p>It is not meant to be your index view, rather a default handler for your website.<br> If you understand the framework, you would like to clear this content and have some dynamic logic handling your pages/views</p>
