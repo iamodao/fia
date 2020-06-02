2b220210e7408311c025950102adfb5d8722f347
Anthony O. Osawere <anthony@osawere.com>
2020-06-02 15:11:36 +0100

    Included router call

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ $oInit['FileInit'] = $oInit['RD'].'/source/fia.init.php';
 if(!file_exists($oInit['FileInit'])){exit('PATH::Missing [Initializer Required]');}
 require $oInit['FileInit'];
 if(file_exists('sandbox.php')){require 'sandbox.php';}
-if(isset($fia) && class_exists('fia') && method_exists('fia', 'router')){$fia->orouter();}
+if(isset($fia) && class_exists('fia') && method_exists('fia', 'orouter')){$fia->orouter();}
 ?>
