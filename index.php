b4124fe883b0f96b1f1c4f47191bb7074968655c
Anthony O. Osawere <anthony@osawere.com>
2020-06-02 21:17:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,5 +8,9 @@ $oInit['FileInit'] = $oInit['RD'].DS.'source'.DS.'fia.init.php';
 if(!file_exists($oInit['FileInit'])){exit('PATH::Missing [Initializer Required]');}
 require $oInit['FileInit'];
 if(file_exists('sandbox.php')){require 'sandbox.php';}
-if(isset($fia) && class_exists('fia') && method_exists('fia', 'orouter')){$fia->orouter();}
+if(isset($fia) && class_exists('fia') && method_exists('fia', 'orouter')){
+	# NOTE ~ when for website [oSITE], for app & api - using auto controllers (oAUTO), or set manually (oGET)
+	$projectAs = 'oAUTO';
+	$fia->orouter($projectAs);
+}
 ?>
