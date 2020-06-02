d495175a0cc18388d1913f7d26c016a4e5089646
Anthony O. Osawere <anthony@osawere.com>
2020-06-02 16:08:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
+# Define Separator
+defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+defined('PS') ? null : define('PS', '/');
+
 $oInit['RD'] = __DIR__;
-$oInit['FileInit'] = $oInit['RD'].'/source/fia.init.php';
+$oInit['FileInit'] = $oInit['RD'].DS.'source'.DS.'fia.init.php';
 if(!file_exists($oInit['FileInit'])){exit('PATH::Missing [Initializer Required]');}
 require $oInit['FileInit'];
 if(file_exists('sandbox.php')){require 'sandbox.php';}
