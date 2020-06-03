1c2a8e6bdf68c38fb4e7ce508598cdb346830c39
Anthony O. Osawere <anthony@osawere.com>
2020-06-03 11:26:32 +0100

    Update fia_db.sql

--- source/asset/fia_db.sql
+++ source/asset/fia_db.sql
@@ -1,15 +1,4 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.7.24 - MySQL Community Server (GPL)
--- Server OS:                    Win64
--- HeidiSQL Version:             11.0.0.5991
--- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
 -- Dumping database structure for fia_db
@@ -71,6 +60,3 @@ REPLACE INTO `firm` (`id`, `buid`, `name`, `email`, `phone`, `address`, `address
 	(32, NULL, 'Jerry Gyang', 'info@jerry.com', NULL, NULL, NULL, NULL, 'Europe/London');
 /*!40000 ALTER TABLE `firm` ENABLE KEYS */;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
