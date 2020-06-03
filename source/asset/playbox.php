8d434495822c85ab7ac08a0d2abc863326290f72
Anthony O. Osawere <anthony@osawere.com>
2020-06-03 13:02:03 +0100

    Update playbox.php

--- source/asset/playbox.php
+++ source/asset/playbox.php
@@ -1,28 +1,14 @@
 <?php
-// $fia->dump($fia->dbo_query('SELECT * FROM `firm` WHERE `id` > 30'));
-#$query = $fia->dbo_query('SELECT * FROM `firm` WHERE `id` > 30');
+echo '<b>QUERY DEBURGING'.'</b><br>';
 
-// $fetch = $query->fetch(PDO::FETCH_ASSOC);
-// $record = $query['oRESULT'];
-// foreach ($record as $key => $row){
-// 	foreach ($row as $label => $value) {
-// 		echo strtoupper($label).': '.$value.'<br>';
-// 	}
-// 	echo '<hr>';
-// }
+#$demo = $fia->resetSQL('firm', 'id');
 
+$qry = "DELETE FROM `firm` WHERE `id` > 30";
+$qry = "INSERT INTO `firm` (`name`) VALUES ('Jerry Gyang')";
+$qry = "UPDATE `firm` SET `email` = 'info@jerry.com' WHERE `id` > 31";
+$qry = "SELECT * FROM `firm` WHERE `id` > 50";
 
-// $demo = $fia->resetPKSQL('firm', 'id');
-// $q = "INSERT INTO `firm` (`name`) VALUES ('Jerry Gyang')";
-// $q = "UPDATE `firm` SET `email` = 'info@jerry.com' WHERE `id` > 31";
-// $q = "DELETE FROM `firm` WHERE `id` > 30";
-$q = "SELECT * FROM `firmz` WHERE `id` > 31";
-$demo = $fia->runSQL($q);
+$demo = $fia->querySQL($qry);
 $fia->dump($demo);
 exit();
-
-
-// $demo = $fia->orouter('oGET');
-// $demo = array_merge($demo, $fia->captureData('oGET'));
-// $fia->dump($demo);
 ?>
