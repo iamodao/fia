17a881ad1f6d0d151e026f30312ce30b24428833
Anthony O. Osawere <anthony@osawere.com>
2020-06-04 22:29:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 defined('PS') ? null : define('PS', '/');
 
 $oInit['RD'] = __DIR__;
+
 $oInit['FileInit'] = $oInit['RD'].DS.'source'.DS.'fia.init.php';
 if(!file_exists($oInit['FileInit'])){exit('PATH::Missing [Initializer Required]');}
 require $oInit['FileInit'];
@@ -11,8 +12,8 @@ require $oInit['FileInit'];
 # Include sandbox for development, demo & testing
 if(file_exists('_ignore/dev5/index.php')){require '_ignore/dev5/index.php';}
 
-if(isset($fia) && class_exists('fia') && method_exists('fia', 'orouter')){
 	# NOTE ~ when for website [oSITE], for app & api - using auto controllers (oAUTO), or set manually (oGET)
+if(isset($fia) && class_exists('fia') && method_exists('fia', 'orouter')){
 	$projectAs = 'oAUTO';
 	$fia->orouter($projectAs);
 }
