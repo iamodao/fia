262d78849ad9fae606889a4125ca05d84cdb11d2
Anthony O. Osawere <anthony@osawere.com>
2020-06-04 22:23:52 +0100

    Create index.php

---
+++ _ignore/dev5/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+	<title>oDEV5 SANDBOX</title>
+	<link rel="shortcut icon" href="./favicon.ico">
+	<link href="" type="text/css" rel="stylesheet" media="all">
+</head>
+<body>
+	<?php
+	$demo = 'SANDBOX';
+	$fia->dump($demo);
+	?>
+	<script src="" type="text/javascript"></script>
+</body>
+</html>
+<?php exit(); ?>
