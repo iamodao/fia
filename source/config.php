306f7bbb1c1a1b4a5bff0dc1e977ef6daab80dc2
Anthony O. Osawere <anthony@osawere.com>
2020-06-05 23:12:43 +0100

    Refined

--- source/config.php
+++ source/config.php
@@ -1,25 +1,33 @@
 <?php
-$oInitConfig = array(
-	'project' => array(
-		'firm' => 'FIA Framework by OA™',
-		'name' => 'FIA Framework',
-		'brand' => 'FIA™',
-		'url' => 'https://fia.go',
-		'domain' => 'fia.go',
-		'email' => 'admin@fia.go',
-		'phone' => '+44 123-456-7890',
-		'version' => 'evolving',
-		'as' => 'oAUTO', # [oAUTO | oSITE]
-	),
-	'database' => array (
-		'host' => 'localhost',
-		'name' => 'fia_db',
-		'user' => 'fia',
-		'password' => 'FIA'
-	),
+#GENERAL CONFIGURATION
+$o_config = array(
 	'machine' => 'oDev',
 	'status' => 'oLive',
 	'https' => 'impose',
 	'timezone' => 'Europe/London',
 );
+
+
+#PROJECT INFORMATION
+$o_config['oPROJECT'] = array(
+	'firm' => 'VERI8™ Inc.',
+	'name' => 'FIA Framework',
+	'brand' => 'FIA™',
+	'domain' => 'fia.go',
+	'url' => 'http://www.fia.go',
+	'email' => 'admin@fia.go',
+	'phone' => '+234 (0) 809-123-7654',
+	'version' => 'eVOLVE',
+	'routing' => 'oAUTO',
+);
+
+
+#DATABASE INFORMATION
+$o_config['oDATABASE'] = array(
+	'host' => 'localhost',
+	'name' => 'fia_db',
+	'user' => 'fia',
+	'password' => 'FIA'
+);
+//NOTE: $o_config['oDATABASE'] triggers DB connection by default
 ?>
