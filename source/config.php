f96e5ac6af365cfcf3fef5be0293c89cad58f5d0
Anthony O. Osawere <anthony@osawere.com>
2020-06-05 19:41:46 +0100

    naming convention redefined

--- source/config.php
+++ source/config.php
@@ -4,11 +4,12 @@ $oInitConfig = array(
 		'firm' => 'FIA Framework by OA™',
 		'name' => 'FIA Framework',
 		'brand' => 'FIA™',
-		'baseURL' => 'https://fia.go',
+		'url' => 'https://fia.go',
 		'domain' => 'fia.go',
 		'email' => 'admin@fia.go',
 		'phone' => '+44 123-456-7890',
 		'version' => 'evolving',
+		'as' => 'oAUTO', # [oAUTO | oSITE]
 	),
 	'database' => array (
 		'host' => 'localhost',
@@ -16,9 +17,9 @@ $oInitConfig = array(
 		'user' => 'fia',
 		'password' => 'FIA'
 	),
-	'isIn' => 'oDev',
-	'isCond' => 'oLive',
-	'imposeSSL' => 'oYeah',
+	'machine' => 'oDev',
+	'status' => 'oLive',
+	'https' => 'impose',
 	'timezone' => 'Europe/London',
 );
 ?>
