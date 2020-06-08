551fd286fcd4fc27eafbe990676ab64ded4e723e
Anthony O. Osawere <anthony@osawere.com>
2020-06-08 10:45:40 +0100

    Update app.php

--- source/module/app.php
+++ source/module/app.php
@@ -1,5 +1,17 @@
 <?php
 // app primary controller
 class oAPP {
+	public function __construct(){
+		$method = fia::route();
+		if(!empty($method) && method_exists(__CLASS__, $method)){
+			return $this->$method();
+		}
+	}
+
+	public function index(){
+		echo 'INDEX on APP';
+	}
 }
+
+$runApp = new oAPP;
 ?>
