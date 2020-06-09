d333d848309c181583cf5d5b84b6d82eb470990a
Anthony O. Osawere <anthony@osawere.com>
2020-06-09 15:01:54 +0100

    Update fia.php

--- fia/fia.php
+++ fia/fia.php
@@ -8,118 +8,37 @@ class fia {
 	#DEFINE PROPERTIES
 	private static $instance;
 
-	public static $timezone;
 	public static $firm;
 	public static $name;
 	public static $brand;
 	public static $acronym;
 	public static $slogan;
-	public static $domain;
 	public static $url;
+	public static $domain;
 	public static $email;
 	public static $phone;
-	public static $version;
 	public static $routing;
-	public static $dbo;
+	public static $version;
+	public static $author;
 	public static $path;
+	public static $dbo;
+	public static $timezone;
 	public static $machine;
 
 
-	#PREVENT MULTIPLE INSTANCE
-	private function __construct(){}
 
 
-	#PREVENT DUPLICATE INSTANCE
-	private function __clone(){}
 
 
-	#INSTANTIATE ~ return instance of static class
-	public static function instantiate($o){
-		if(is_null(self::$instance)){
-			self::$instance = new self();
-			self::initialize($o);
-		}
-		return self::$instance;
-	}
 
 
-	#RETURNS BOOLEAN ON HTTPS CHECK
-	public static function https(){
-		$o = false; $https = 'inactive'; $port = 'default';
-		if(!empty($_SERVER['HTTPS'])){$https = $_SERVER['HTTPS'];}
-		if($https !== 'inactive'){$https == 'active';}
-		if(!empty($_SERVER['SERVER_PORT'])){$port = $_SERVER['SERVER_PORT'];}
-		if($https == 'active' || $port == 443){$o = true;}
-		elseif(!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'){$o = true;}
-		elseif(!empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on'){$o = true;}
-		return $o;
-	}
 
 
-	#IMPOSE SSL on URL ~  also starts session
-	public static function imposeSSL($url='', $move='oNOPE'){
-		self::sessionStart();
-		if(empty($_SESSION['oSSL']) || $_SESSION['oSSL'] !== 'imposed'){
-			$protocol = self::https() ? 'https' : 'http';
-			if($protocol !== 'https'){
-				$o = 'https://';
-				if(!empty($url)){$o .= $url;}
-				else {
-					if(!empty($_SERVER['HTTP_HOST'])){$o .= $_SERVER['HTTP_HOST'];}
-					if(!empty($_SERVER['REQUEST_URI'])){$o .= $_SERVER['REQUEST_URI'];}
-				}
-				if(filter_var($o, FILTER_VALIDATE_URL) !== false){
-					$_SESSION['oSSL'] = 'imposed';
-					if($move == 'oYEAH'){header('HTTP/1.1 301 Moved Permanently');}
-					header('Location: ' . $o);
-					exit;
-				}
-			}
-		}
-	}
 
 
-	#SET TIMEZONE & RETURN BOOLEAN
-	public static function timezone($i='oFIA'){
-		if($i == 'oFIA' || empty($i)){$i = 'Africa/Lagos';}
-		$o = date_default_timezone_set($i);
-		return $o;
-	}
 
 
-	#SET BASE DOMAIN & ASSIGN TO PROPERTY
-	protected static function domain(){
-	}
 
-
-	#SET BASE URL & ASSIGN TO PROPERTY
-	protected static function url(){
-	}
-
-
-	#SET PROJECT INFORMATION AS OBJECT PROPERTIES
-	private static function project($o){
-		if(isset($o['oPROJECT']) && !empty($o['oPROJECT']) && is_array($o['oPROJECT'])){
-			$project = $o['oPROJECT'];
-			foreach ($project as $key => $value){
-				if(property_exists(__CLASS__, $key) && !empty($value)){
-					self::${$key} = $value;
-					unset($project[$key]);
-				}
-				elseif($key == 'domain' && empty($value)){
-					self::domain();
-					unset($project['domain']);
-				}
-				elseif($key == 'url' && empty($value)){
-					self::url();
-					unset($project['url']);
-				}
-			}
-			unset($o['oPROJECT']);
-			if(!empty($project)){$o['oPROJECT'] = $project;}
-		}
-		return $o;
-	}
 
 
 	#SET PATH TO DIRECTORIES
@@ -173,61 +92,7 @@ class fia {
 	}
 
 
-	#INITIALIZATION ~ to initialize application
-	public static function initialize($o){
-		if(!empty($o) && is_array($o)){
 
-			#Session
-			if(!empty($o['session'])){
-				self::sessionName($o['session']);
-				unset($o['session']);
-			}
-			self::sessionStart();
-
-
-			#Enforce https
-			if(array_key_exists('https', $o) && $o['https'] == 'impose'){
-				self::imposeSSL();
-				unset($o['https']);
-			}
-
-			#check status & respond (-set error_reporting)
-			if(!empty($o['status'])){
-				if($o['status'] != 'oLIVE'){oExit('project', 'offline!', 'Sorry, this project is offline at the moment');}
-				unset($o['status']);
-			}
-
-			#Set machine & TODO -set error_reporting
-			if(!empty($o['machine'])){
-				self::$machine = $o['machine'];
-				unset($o['machine']);
-			}
-
-
-			#Set timezone
-			if(array_key_exists('timezone', $o)){
-				$timezone = self::timezone($o['timezone']);
-				if($timezone !== false){self::$timezone = $o['timezone'];}
-				if(!empty(self::$timezone)){unset($o['timezone']);}
-			}
-			if(empty(self::$timezone)){
-				self::$timezone = 'Africa/Lagos';
-				self::timezone(self::$timezone);
-			}
-
-			#Set project information
-			$o = self::project($o);
-
-			#set database
-			if(array_key_exists('oDATABASE', $o)){
-				self::database($o['oDATABASE']);
-				unset($o['oDATABASE']);
-			}
-
-			#Set path
-			$o = self::path($o);
-		}
-	}
 
 
 
@@ -974,8 +839,220 @@ class fia {
 
 
 
-	//=====::BEGIN SESSION UTILITY::=====//
 
+
+
+
+
+
+
+
+	/**=====::LANGUAGE UTILITY::=====**/
+
+	#GET & SET LANGUAGE
+	public static function lang($lang=''){
+		if(!empty($lang)){$o = $lang;}
+		elseif(!empty($_GET['lang'])){$o = $_GET['lang'];}
+		elseif(!empty($_POST['oLang'])){$o = $_POST['oLang'];}
+		elseif(!empty($_SESSION['oLANG'])){$o = $_SESSION['oLANG'];}
+		else {$o = 'en';}
+
+		if(empty($_SESSION['oLANG'])){
+			self::sessionStart();
+			$_SESSION['oLANG'] = $o;
+		}
+		elseif($_SESSION['oLANG'] != $o){$_SESSION['oLANG'] = $o;}
+		return strtolower($o);
+	}
+
+
+
+
+
+
+
+
+
+
+	/**=====::JSON UTILITY::=====**/
+
+
+	#JSON ERROR INTERPRETER
+	public static function jsonError($e, $i=''){
+		$o['JSON_INPUT'] = $i; $o['JSON_ERROR'] = $e;
+		if($e == 4){$o['JSON_ERROR_MSG'] = 'Syntax Error';}
+		elseif($e == 5){$o['JSON_ERROR_MSG'] = 'Malformed UTF-8 characters, possibly incorrectly encoded';}
+		if(!empty($o)){return $o;}
+	}
+
+
+
+	#PERFORM JSON OPERATIONS (encode, decode & print)
+	public static function json($input, $i='oENCODE'){
+		if(!empty($input)){
+			if($i == 'oENCODE'){$o = json_encode($input);}
+			elseif($i == 'oDECODE'){$o = json_decode($input);}
+			$e = json_last_error();
+			if(!empty($e)){return self::jsonError($e, $input);}
+			elseif($i == 'oENCODE' || $i == 'oDECODE'){return $o;}
+			elseif($i == 'oPRINT'){
+				header('Content-Type: application/json');
+				echo json_encode($input, JSON_PRETTY_PRINT);
+				exit;
+			}
+		}
+		return;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	#PREVENT MULTIPLE INSTANCE
+	private function __construct(){}
+
+
+
+	#PREVENT DUPLICATE INSTANCE
+	private function __clone(){}
+
+
+
+	#INSTANTIATE ~ return instance of static class
+	public static function instantiate($o){
+		if(is_null(self::$instance)){
+			self::$instance = new self();
+			self::initialize($o);
+		}
+		return self::$instance;
+	}
+
+
+
+
+
+
+
+
+
+
+	//=====::INITIALIZATION UTILITY::=====//
+
+	#INITIALIZATION ~ to initialize application
+	public static function initialize($o){
+		if(!empty($o) && is_array($o)){
+
+			#session
+			if(!empty($o['session'])){
+				self::sessionName($o['session']);
+				unset($o['session']);
+			}
+			self::sessionStart();
+
+			#enforce https
+			if(array_key_exists('https', $o) && $o['https'] == 'impose'){
+				self::imposeSSL();
+				unset($o['https']);
+			}
+
+			#check status & respond (-set error_reporting)
+			if(!empty($o['status'])){
+				if($o['status'] != 'oLIVE'){oExit('project', 'offline!', 'Sorry, this project is offline at the moment');}
+				unset($o['status']);
+			}
+
+			#set machine (TODO -set error_reporting)
+			if(!empty($o['machine'])){
+				self::$machine = $o['machine'];
+				unset($o['machine']);
+			}
+
+
+			#set timezone
+			if(array_key_exists('timezone', $o)){
+				$timezone = self::timezone($o['timezone']);
+				if($timezone !== false){self::$timezone = $o['timezone'];}
+				if(!empty(self::$timezone)){unset($o['timezone']);}
+			}
+			if(empty(self::$timezone)){
+				self::$timezone = 'Africa/Lagos';
+				self::timezone(self::$timezone);
+			}
+
+			#set project information
+			$o = self::project($o);
+
+			#set database
+			if(array_key_exists('oDATABASE', $o)){
+				self::database($o['oDATABASE']);
+				unset($o['oDATABASE']);
+			}
+
+			#set path
+			$o = self::path($o);
+		}
+	}
+
+
+
+	#SET PROJECT INFORMATION AS OBJECT PROPERTIES
+	private static function project($o){
+		if(isset($o['oPROJECT']) && !empty($o['oPROJECT']) && is_array($o['oPROJECT'])){
+			$project = $o['oPROJECT'];
+			foreach ($project as $key => $value){
+				if(property_exists(__CLASS__, $key) && !empty($value)){
+					self::${$key} = $value;
+					unset($project[$key]);
+				}
+				elseif($key == 'domain' && empty($value)){
+					self::domain();
+					unset($project['domain']);
+				}
+				elseif($key == 'url' && empty($value)){
+					self::url();
+					unset($project['url']);
+				}
+			}
+			unset($o['oPROJECT']);
+			if(!empty($project)){$o['oPROJECT'] = $project;}
+		}
+		return $o;
+	}
+
+
+
+
+
+
+
+
+	//=====::BEGIN SESSION UTILITY::=====//
 
 	#RETURN SESSION VALUE - $_SESSION['value']
 	public static function session($i='', $v='', $id=''){
@@ -1130,82 +1207,46 @@ class fia {
 
 
 
-	/**=====::LANGUAGE UTILITY::=====**/
 
-	#GET & SET LANGUAGE
-	public static function lang($lang=''){
-		if(!empty($lang)){$o = $lang;}
-		elseif(!empty($_GET['lang'])){$o = $_GET['lang'];}
-		elseif(!empty($_POST['oLang'])){$o = $_POST['oLang'];}
-		elseif(!empty($_SESSION['oLANG'])){$o = $_SESSION['oLANG'];}
-		else {$o = 'en';}
 
-		if(empty($_SESSION['oLANG'])){
-			self::sessionStart();
-			$_SESSION['oLANG'] = $o;
-		}
-		elseif($_SESSION['oLANG'] != $o){$_SESSION['oLANG'] = $o;}
-		return strtolower($o);
+
+	//=====::SSL UTILITY::=====//
+
+	#RETURNS BOOLEAN ON HTTPS CHECK
+	public static function https(){
+		$o = false; $https = 'inactive'; $port = 'default';
+		if(!empty($_SERVER['HTTPS'])){$https = $_SERVER['HTTPS'];}
+		if($https !== 'inactive'){$https == 'active';}
+		if(!empty($_SERVER['SERVER_PORT'])){$port = $_SERVER['SERVER_PORT'];}
+		if($https == 'active' || $port == 443){$o = true;}
+		elseif(!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'){$o = true;}
+		elseif(!empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on'){$o = true;}
+		return $o;
 	}
 
 
 
-
-
-
-
-
-
-
-	/**=====::JSON UTILITY::=====**/
-
-
-	#JSON ERROR INTERPRETER
-	public static function jsonError($e, $i=''){
-		$o['JSON_INPUT'] = $i; $o['JSON_ERROR'] = $e;
-		if($e == 4){$o['JSON_ERROR_MSG'] = 'Syntax Error';}
-		elseif($e == 5){$o['JSON_ERROR_MSG'] = 'Malformed UTF-8 characters, possibly incorrectly encoded';}
-		if(!empty($o)){return $o;}
-	}
-
-
-
-	#PERFORM JSON OPERATIONS (encode, decode & print)
-	public static function json($input, $i='oENCODE'){
-		if(!empty($input)){
-			if($i == 'oENCODE'){$o = json_encode($input);}
-			elseif($i == 'oDECODE'){$o = json_decode($input);}
-			$e = json_last_error();
-			if(!empty($e)){return self::jsonError($e, $input);}
-			elseif($i == 'oENCODE' || $i == 'oDECODE'){return $o;}
-			elseif($i == 'oPRINT'){
-				header('Content-Type: application/json');
-				echo json_encode($input, JSON_PRETTY_PRINT);
-				exit;
+	#IMPOSE SSL on URL ~  also starts session
+	public static function imposeSSL($url='', $move='oNOPE'){
+		self::sessionStart();
+		if(empty($_SESSION['oSSL']) || $_SESSION['oSSL'] !== 'imposed'){
+			$protocol = self::https() ? 'https' : 'http';
+			if($protocol !== 'https'){
+				$o = 'https://';
+				if(!empty($url)){$o .= $url;}
+				else {
+					if(!empty($_SERVER['HTTP_HOST'])){$o .= $_SERVER['HTTP_HOST'];}
+					if(!empty($_SERVER['REQUEST_URI'])){$o .= $_SERVER['REQUEST_URI'];}
+				}
+				if(filter_var($o, FILTER_VALIDATE_URL) !== false){
+					$_SESSION['oSSL'] = 'imposed';
+					if($move == 'oYEAH'){header('HTTP/1.1 301 Moved Permanently');}
+					header('Location: ' . $o);
+					exit;
+				}
 			}
 		}
-		return;
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -1219,12 +1260,42 @@ class fia {
 
 	//=====::MISCELLANEOUS UTILITY::=====//
 
+	#SET TIMEZONE & RETURN BOOLEAN
+	public static function timezone($i='oFIA'){
+		if($i == 'oFIA' || empty($i)){$i = 'Africa/Lagos';}
+		$o = date_default_timezone_set($i);
+		return $o;
+	}
+
+
 
 	#STATIC COUNTER
 	public static function counter($i=''){
 		static $counter = 0; #on first run
 		if(!empty($i)){$counter = $counter + $i;}
     return $counter++; #returns counter + 1
+  }
+
+
+
+	#SET/GET BASE DOMAIN & ASSIGN TO PROPERTY
+  public static function domain($i='oGET'){
+  	$o = self::base('oDOMAIN');
+  	if($i == 'oGET'){
+  		if(isset(self::$domain)){
+  			return self::$doman;
+  		}
+  		return $o;
+  	}
+  	elseif($i == 'oSET'){
+  		$o = self::$domain;
+  	}
+  }
+
+
+
+	#SET/GET BASE URL & ASSIGN TO PROPERTY
+  public static function url($i='oGET'){
   }
 
 
