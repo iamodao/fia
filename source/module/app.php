e699d882783bce25ed6670990981cb8eb3568096
Anthony O. Osawere <anthony@osawere.com>
2020-06-13 13:17:13 +0100

    Update app.php

--- source/module/app.php
+++ source/module/app.php
@@ -1,17 +1,132 @@
 <?php
 // app primary controller
 class oAPP {
+	var $method;
+
 	public function __construct(){
 		$method = fia::route();
 		if(!empty($method) && method_exists(__CLASS__, $method)){
+			$this->method = $method;
+			fia::sessionStart();
+			$noauthroute = array('login', 'logout', 'index');
+			$exemptroute = fia::routeExempt($noauthroute);
+			if(!$exemptroute){
+				$this->Auth_IsLoggedIn();
+			}
+			fia::code('booking');
 			return $this->$method();
 		}
 	}
 
+	public function demo(){}
 	public function index(){
-		echo 'INDEX on APP';
+		self::Auth_IsLoggedIn('login');
+		fia::exitTo('dashboard', 'oRELATIVE');
 	}
+
+	public function logout(){
+		$this->Auth_Logout();
+	}
+
+	public function login(){
+		if(fia::routeAction() == 'process'){
+			if(!empty($_POST)){
+				fia::formData('oPOST', 'login');
+				if(!self::Auth_Login()){
+					fia::exitTo('login?oact=login-failed', 'oRELATIVE');
+				}
+				else {
+					fia::sessionUnset('oFORM_POST_DATA');
+					fia::exitTo('dashboard', 'oRELATIVE');
+				}
+			}
+			else {
+				fia::exitTo('login', 'oRELATIVE');
+			}
+		}
+		fia::theme('auth');
+		fia::sessionUnset('active_user_bind');
+		fia::sessionUnset('is_logged_in');
+	}
+
+
+	public function dashboard(){}
+
+
+
+
+
+
+
+	private function Auth_IsLoggedIn($link=''){
+		if(fia::session('is_logged_in') !== 'yez'){
+			if(empty($link)){
+				fia::exitTo('login?oact=not-logged-in', 'oRELATIVE');
+			}
+			else {
+				fia::exitTo($link, 'oRELATIVE');
+			}
+		}
+	}
+
+
+	private function Auth_Login(){
+		$field = array('userid', 'password');
+		$record = fia::dataRecord('oPOST', $field);
+		$column = fia::arrayBind($record);
+		if($record !== false){
+			$query = "SELECT `bind` FROM `user` WHERE `username` = :userid AND `password` = :password LIMIT 1";
+			$result = fia::runSQL($query, $column, 'oRECORD');
+			if(fia::isRecordSQL($result, 'bind')){
+				fia::session('active_user_bind', $result['bind']);
+				fia::session('is_logged_in', 'yez');
+				return true;
+			}
+		}
+		else {
+			fia::sessionUnset('active_user_bind');
+			fia::sessionUnset('is_logged_in');
+		}
+		return false;
+	}
+
+	private function Auth_Logout(){
+		fia::sessionUnset('oFORM_POST_DATA');
+		fia::sessionUnset('active_user_bind');
+		fia::sessionUnset('is_logged_in');
+		fia::sessionFresh();
+		fia::exitTo('login?oact=logged-out', 'oRELATIVE');
+	}
+
+
+	private function Auth_User(){
+		$query = "SELECT * FROM `user` WHERE `bind` = '".fia::session('active_user_bind')."' LIMIT 1";
+		$result = fia::runSQL($query);
+		if(fia::isRecordSQL($result, 'oRECORD')){
+			return $result['oRECORD'];
+		}
+		return false;
+	}
+
+
+	private function Auth_RestrictTo($type='', $redirect=''){
+		#self::Auth_IsLoggedIn();
+		$user = self::Auth_User();
+		$restrict = 'no';
+		if(!empty($type) && !empty($user['type'])){
+			if(!is_array($type) && $type != $user['type']){$restrict = 'yez';}
+			elseif(is_array($type) && !in_array($user['type'], $type)){$restrict = 'yez';}
+		}
+		if($restrict == 'yez'){
+			if(!empty($redirect)){fia::exitTo($redirect, 'oRELATIVE');}
+			else {fia::exitTo('login', 'oRELATIVE');}
+		}
+	}
+
+
+
 }
+
 
 $runApp = new oAPP;
 ?>
