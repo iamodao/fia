c4214edd07babeca041e2702c86876d9c6c8ee45
Anthony O. Osawere <anthony@osawere.com>
2020-06-21 12:26:56 +0100

    Update menu.php

--- source/layout/bit/nav/menu.php
+++ source/layout/bit/nav/menu.php
@@ -1,14 +1,7 @@
 <li class="mdc-list-item" role="menuitem">
-	<div class="item-thumbnail item-thumbnail-icon-only"><i class="mdi mdi-account-edit-outline text-primary"></i></div>
+	<div class="item-thumbnail item-thumbnail-icon-only"> <i class="mdi mdi-account-edit text-primary"></i> </div>
 	<div class="item-content d-flex align-items-start flex-column justify-content-center">
-		<h6 class="item-subject font-weight-normal" onClick="window.location.href='/update-profile';">Update Profile</h6>
-	</div>
-</li>
-
-<li class="mdc-list-item" role="menuitem">
-	<div class="item-thumbnail item-thumbnail-icon-only"> <i class="mdi mdi-lock-outline text-primary"></i> </div>
-	<div class="item-content d-flex align-items-start flex-column justify-content-center">
-		<h6 class="item-subject font-weight-normal" onClick="window.location.href='/change-password';">Change Password</h6>
+		<h6 class="item-subject font-weight-normal" onClick="window.location.href='/settings';">Settings</h6>
 	</div>
 </li>
 <li class="mdc-list-item" role="menuitem">
