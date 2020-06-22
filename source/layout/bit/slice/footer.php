906bd10ea18e6b1dab0677b7d56d08907991d7cb
Anthony O. Osawere <anthony@osawere.com>
2020-06-22 17:45:52 +0100

    Update footer.php

--- source/layout/bit/slice/footer.php
+++ source/layout/bit/slice/footer.php
@@ -1,10 +1,10 @@
 <footer>
-  <div class="mdc-layout-grid">
-    <div class="mdc-layout-grid__inner">
-      <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-6-desktop"> <span class="tx-14">Copyright © <?php echo date('Y');?> <a href="#" target="_blank">Bremit Booking</a>. All rights reserved.</span> </div>
-      <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-6-desktop d-flex justify-content-end">
-        <div class="d-flex align-items-center"> <a href="#">Home</a> <span class="vertical-divider"></span> <a href="#">BremitCS</a> </div>
-      </div>
-    </div>
-  </div>
+	<div class="mdc-layout-grid">
+		<div class="mdc-layout-grid__inner">
+			<div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-6-desktop"> <span class="tx-14">Copyright © <?php echo date('Y');?> <a href="#" target="_blank">Bremit oBMS</a>. All rights reserved.</span> </div>
+			<div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-6-desktop d-flex justify-content-end">
+				<div class="d-flex align-items-center"> <a href="#">Home</a> <span class="vertical-divider"></span> <a href="https://bremitcs.com" target="_blank">BremitCS</a> </div>
+			</div>
+		</div>
+	</div>
 </footer>
