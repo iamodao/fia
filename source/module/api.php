bf54b6c6bd94bd1e632af9c5370529cfb19dc2cb
Anthony O. Osawere <anthony@osawere.com>
2020-06-22 03:11:22 +0100

    Update api.php

--- source/module/api.php
+++ source/module/api.php
@@ -1,5 +1,16 @@
 <?php
-// api primary controller
+// The default API controller
 class oAPI {
+	var $method;
+	public function __construct(){
+		$this->check_controller();
+	}
+
+	private function check_controller(){
+		$method = fia::stringTo(fia::route('oAPI'), 'oMETHOD');
+		if(empty($method) || !method_exists(__CLASS__, $method)){oExit('api', $method, 'controller required');}
+		$this->method = $method;
+	}
 }
+$runAPI = new oAPI;
 ?>
