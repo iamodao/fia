be00d38aaeeb98d95af06905be42084152d08154
Anthony O. Osawere <anthony@osawere.com>
2020-06-22 17:45:39 +0100

    Update head.php

--- source/layout/bit/head.php
+++ source/layout/bit/head.php
@@ -2,7 +2,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>Bremit Booking<?php if(fia::route() != 'index'){echo ' - '.fia::stringTo(fia::route(), 'oTITLE');} else {echo ' App';}?></title>
+<title>Bremit oBMS<?php if(fia::route() != 'index'){echo ' - '.fia::stringTo(fia::route(), 'oTITLE');} else {echo ' App';}?></title>
 <script src="/asset/js/fontawesome.min.js"></script>
 <link rel="stylesheet" href="/asset/vendors/mdi/css/materialdesignicons.min.css">
 <link rel="stylesheet" href="/asset/vendors/css/vendor.bundle.base.css">
