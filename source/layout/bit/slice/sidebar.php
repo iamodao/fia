f926380820d9ec57ae6f6e731f9f8638d0105991
Anthony O. Osawere <anthony@osawere.com>
2020-06-25 05:31:28 +0100

    Update sidebar.php

--- source/layout/bit/slice/sidebar.php
+++ source/layout/bit/slice/sidebar.php
@@ -1,35 +1,67 @@
-<aside class="mdc-drawer mdc-drawer--dismissible mdc-drawer--open">
-	<div class="mdc-drawer__header">
-		<a href="/" class="brand-logo"><img src="/asset/images/logo.png" alt="logo"></a>
-	</div>
-	<div class="mdc-drawer__content">
-		<div class="mdc-list-group">
-			<nav class="mdc-list mdc-drawer-menu">
-				<div class="mdc-list-item mdc-drawer-item">
-					<a class="mdc-drawer-link" href="/dashboard">
-						<i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">home</i> Dashboard
-					</a>
+
+<div id="layoutSidenav_nav">
+	<nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+		<div class="sb-sidenav-menu">
+			<div class="nav">
+				<div class="sb-sidenav-menu-heading">Navigation</div>
+				<a class="nav-link" href="dashboard"> <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>	Dashboard	</a>
+				<div class="sb-sidenav-menu-heading">Interface</div>
+				<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+					<div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+					Layouts
+					<div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+				</a>
+				<div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+					<nav class="sb-sidenav-menu-nested nav">
+						<a class="nav-link" href="layout-static.html">Static Navigation</a>
+						<a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
+					</nav>
 				</div>
-				<div class="mdc-list-item mdc-drawer-item">
-					<a class="mdc-drawer-link" href="/booking">
-						<i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">pages</i> Booking
-					</a>
+				<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+					<div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+					Pages
+					<div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+				</a>
+				<div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-parent="#sidenavAccordion">
+					<nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+						<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
+							Authentication
+							<div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+						</a>
+						<div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-parent="#sidenavAccordionPages">
+							<nav class="sb-sidenav-menu-nested nav">
+								<a class="nav-link" href="login.html">Login</a>
+								<a class="nav-link" href="register.html">Register</a>
+								<a class="nav-link" href="password.html">Forgot Password</a>
+							</nav>
+						</div>
+						<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
+							Error
+							<div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+						</a>
+						<div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-parent="#sidenavAccordionPages">
+							<nav class="sb-sidenav-menu-nested nav">
+								<a class="nav-link" href="401.html">401 Page</a>
+								<a class="nav-link" href="404.html">404 Page</a>
+								<a class="nav-link" href="500.html">500 Page</a>
+							</nav>
+						</div>
+					</nav>
 				</div>
-				<?php
-				fia::obit('nav'.DS.'reservation');
-				$activeUser = new ActiveUser;
-				if(strtolower($activeUser->type) == 'admin'){
-					fia::obit('nav'.DS.'hotel');
-					fia::obit('nav'.DS.'employee');
-				}
-				?>
-				<div class="mdc-list-item mdc-drawer-item">
-					<a class="mdc-drawer-link" href="/settings"> <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">settings</i> Settings</a>
-				</div>
-				<div class="mdc-list-item mdc-drawer-item">
-					<a class="mdc-drawer-link" href="/logout"><i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">input</i> Logout </a>
-				</div>
-			</nav>
+				<div class="sb-sidenav-menu-heading">Addons</div>
+				<a class="nav-link" href="charts.html">
+					<div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+					Charts
+				</a>
+				<a class="nav-link" href="tables.html">
+					<div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+					Tables
+				</a>
+			</div>
 		</div>
-	</div>
-</aside>
+		<div class="sb-sidenav-footer">
+			<div class="small">Logged in as:</div>
+			Start Bootstrap
+		</div>
+	</nav>
+</div>
