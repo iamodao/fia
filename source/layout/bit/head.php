3e48a2165ea48e0269d3b9d1b29a55968e3c774d
Anthony O. Osawere <anthony@osawere.com>
2020-06-25 02:55:52 +0100

    Update head.php

--- source/layout/bit/head.php
+++ source/layout/bit/head.php
@@ -3,6 +3,6 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>BremitBMS<?php if(fia::route() != 'index'){echo ' - '.oString::to(fia::route(), 'oTITLE');} else {echo ' App';}?></title>
-
 <link href="<?php echo fia::pathTo('ASSET');?>main.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
