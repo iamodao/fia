59b533c3a7003f35c1a8bfa7b7a2af9b97c8a717
Anthony O. Osawere <anthony@osawere.com>
2020-06-25 05:31:18 +0100

    Update js.php

--- source/layout/bit/js.php
+++ source/layout/bit/js.php
@@ -1,12 +1,12 @@
 
-<script src="<?php echo fia::pathTo('ASSET');?>jquery.js" crossorigin="anonymous"></script>
-<script src="<?php echo fia::pathTo('ASSET');?>bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-<script src="<?php echo fia::pathTo('ASSET');?>main.js"></script>
-<script src="<?php echo fia::pathTo('ASSET');?>chart.min.js" crossorigin="anonymous"></script>
-<script src="<?php echo fia::pathTo('ASSET');?>chart-area-demo.js"></script>
-<script src="<?php echo fia::pathTo('ASSET');?>chart-bar-demo.js"></script>
-<script src="<?php echo fia::pathTo('ASSET');?>jquery.dataTables.min.js" crossorigin="anonymous"></script>
-<script src="<?php echo fia::pathTo('ASSET');?>dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+<script src="/asset/jquery.js" crossorigin="anonymous"></script>
+<script src="/asset/bootstrap.js" crossorigin="anonymous"></script>
+<script src="/asset/main.js"></script>
+<script src="/asset/chart.js" crossorigin="anonymous"></script>
+<script src="/asset/chart-area-demo.js"></script>
+<script src="/asset/chart-bar-demo.js"></script>
+<script src="/asset/jquery.datatables.js" crossorigin="anonymous"></script>
+<script src="/asset/datatables.js" crossorigin="anonymous"></script>
 <script>
 	// Call the dataTables jQuery plugin
 	$(document).ready(function() {
