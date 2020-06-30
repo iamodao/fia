e1828958b12bec6b2440155078483449039a68cf
Anthony O. Osawere <anthony@osawere.com>
2020-06-30 11:26:19 +0100

    Update fia_db_now.sql

--- _ignore/dev5/code/fia_db_now.sql
+++ _ignore/dev5/code/fia_db_now.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
--- Host:                         localhost
+-- Host:                         127.0.0.1
 -- Server version:               5.7.24 - MySQL Community Server (GPL)
 -- Server OS:                    Win64
--- HeidiSQL Version:             11.0.0.6013
+-- HeidiSQL Version:             11.0.0.6024
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -35,7 +35,6 @@ CREATE TABLE IF NOT EXISTS `booking` (
   `sex` char(100) DEFAULT NULL,
   `summary` char(200) DEFAULT NULL,
   `amount` bigint(20) DEFAULT '0',
-  `birth` date DEFAULT NULL,
   `schedule` datetime DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `bind` (`bind`) USING BTREE,
@@ -45,12 +44,18 @@ CREATE TABLE IF NOT EXISTS `booking` (
   KEY `title` (`refid`) USING BTREE,
   KEY `type` (`type`),
   KEY `suite` (`suite`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 
--- Dumping data for table fia_db.booking: ~0 rows (approximately)
+-- Dumping data for table fia_db.booking: ~7 rows (approximately)
 /*!40000 ALTER TABLE `booking` DISABLE KEYS */;
-REPLACE INTO `booking` (`id`, `bind`, `entry`, `author`, `status`, `suite`, `refid`, `type`, `name`, `email`, `phone`, `address`, `sex`, `summary`, `amount`, `birth`, `schedule`) VALUES
-	(1, '476159302832742', '2020-06-24 20:52:07', 'APP', 'PENDING', NULL, '2151', 'SALON', 'Jerry Markus', '', '', '', '', 'Hair Cut', 0, NULL, '2020-06-26 09:45:00');
+REPLACE INTO `booking` (`id`, `bind`, `entry`, `author`, `status`, `suite`, `refid`, `type`, `name`, `email`, `phone`, `address`, `sex`, `summary`, `amount`, `schedule`) VALUES
+	(1, '476159302832742', '2020-06-24 20:52:07', 'APP', 'PENDING', NULL, '2151', 'SALON', 'Jerry Markus', '', '', '', '', 'Hair Cut', 0, '2020-06-26 09:45:00'),
+	(2, '525159345419957', '2020-06-29 19:09:59', 'APP', 'PENDING', NULL, '1120', 'SALON', 'Samson Emah', '', '', '', '', '', 0, '2020-10-12 09:45:30'),
+	(3, '301159345425314', '2020-06-29 19:10:53', 'APP', 'PENDING', NULL, '3586', 'CLEANING', 'Kingston Larel', '', '', '', '', '', 0, '2020-10-12 09:45:30'),
+	(4, '585159345462292', '2020-06-29 19:17:02', 'APP', 'PENDING', NULL, '5386', 'CLEANING', 'Derik Leventis', 'derik@leventis.co', '09077886251', '', '', 'Cleaning at my apartment', 0, '2020-06-19 09:48:00'),
+	(5, '569159345750752', '2020-06-29 20:05:07', 'APP', 'PENDING', 'Double', '9330', 'ROOM', 'Vincent Marley', 'vincent@marley.com', '09077886253', '', '', 'I need a double for 2 nights', 0, '2020-07-12 10:23:00'),
+	(6, '626159345848987', '2020-06-29 20:21:29', 'APP', 'PENDING', '', '7314', 'ROOM', 'Lion King', 'lion@king.com', '07037841671', '', '', '', 0, '2020-07-02 02:35:00'),
+	(7, '892159345866611', '2020-06-29 20:24:26', 'APP', 'PENDING', 'Executive', '6026', 'ROOM', 'Cresta Juin', '', '', '', '', 'Best of it all', 0, '2020-07-05 09:45:30');
 /*!40000 ALTER TABLE `booking` ENABLE KEYS */;
 
 -- Dumping structure for table fia_db.room
@@ -100,12 +105,13 @@ CREATE TABLE IF NOT EXISTS `suite` (
   KEY `entry` (`entry`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 
--- Dumping data for table fia_db.suite: ~3 rows (approximately)
+-- Dumping data for table fia_db.suite: ~4 rows (approximately)
 /*!40000 ALTER TABLE `suite` DISABLE KEYS */;
 REPLACE INTO `suite` (`id`, `bind`, `entry`, `author`, `status`, `title`, `price`, `stock`) VALUES
 	(1, '904159301114463', '2020-06-24 16:05:44', 'APP', 'AVAILABLE', 'Standard', 10000, 0),
 	(2, '442159301116415', '2020-06-24 16:06:04', 'APP', 'AVAILABLE', 'Double', 16000, 4),
-	(3, '790159301645356', '2020-06-24 17:34:13', 'APP', 'AVAILABLE', 'Execute', 31500, 0);
+	(3, '790159301645356', '2020-06-24 17:34:13', 'APP', 'AVAILABLE', 'Executive', 31500, 0),
+	(4, '611159346444569', '2020-06-29 22:00:45', 'APP', 'AVAILABLE', 'Single', 8000, 5);
 /*!40000 ALTER TABLE `suite` ENABLE KEYS */;
 
 -- Dumping structure for table fia_db.user
@@ -134,12 +140,12 @@ CREATE TABLE IF NOT EXISTS `user` (
   UNIQUE KEY `phone` (`phone`),
   KEY `type` (`type`),
   KEY `office` (`office`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table fia_db.user: ~10 rows (approximately)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 REPLACE INTO `user` (`id`, `bind`, `author`, `status`, `entry`, `session`, `email`, `phone`, `username`, `password`, `name`, `type`, `office`, `address`, `sex`, `birth`) VALUES
-	(1, '246368795', 'SELF', 'active', '2020-06-21 22:31:40', NULL, 'odao@guy.com', '09097996048', 'odao', 'OneGuy', 'ODAO VAE', 'ADMIN', 'ADMIN', NULL, NULL, NULL),
+	(1, '246368795', 'SELF', 'active', '2020-06-21 22:31:40', NULL, 'odao@vae.com', '09097996048', 'odao', 'onguy', 'ODAO', 'ADMIN', 'ADMIN', NULL, NULL, NULL),
 	(2, '1457580891', 'oAPP', 'active', '2020-06-21 23:23:30', NULL, 'salon@guy.com', '09097996043', 'salon', 'SalonGuy', 'Salon Guy', 'STAFF', 'SALON', NULL, NULL, NULL),
 	(3, '1809771323', 'oAPP', 'active', '2020-06-21 23:26:24', NULL, 'clean@guy.com', '09097996045', 'clean', 'CleanGuy', 'Clean Guy', 'STAFF', 'CLEANING', NULL, NULL, NULL),
 	(4, '448264615', 'oAPP', 'active', '2020-06-21 23:27:40', NULL, 'lounge@guy.com', '09097996047', 'lounge', 'LoungeGuy', 'Lounge Guy', 'STAFF', 'LOUNGE', NULL, NULL, NULL),
@@ -148,7 +154,8 @@ REPLACE INTO `user` (`id`, `bind`, `author`, `status`, `entry`, `session`, `emai
 	(7, NULL, 'APP', 'ACTIVE', '2020-06-24 14:28:13', NULL, 'odao@vae24.co', '09097996051', NULL, NULL, 'ODAO VAE', NULL, NULL, NULL, 'male', NULL),
 	(8, NULL, 'APP', 'ACTIVE', '2020-06-24 14:28:31', NULL, 'odao@vae24.org', '09057996051', NULL, NULL, 'ODAO VAE', NULL, NULL, NULL, 'male', NULL),
 	(10, '159300579486392', 'APP', 'ACTIVE', '2020-06-24 14:36:34', NULL, 'odao@vae24.co.uk', '09057996054', NULL, NULL, 'ODAO VAE', NULL, NULL, NULL, 'male', NULL),
-	(17, '213159300650793', 'APP', 'ACTIVE', '2020-06-24 14:48:27', NULL, 'odao@vae24.ng', '09037996054', NULL, NULL, 'ODAO VAE', NULL, NULL, NULL, 'male', NULL);
+	(17, '213159300650793', 'APP', 'ACTIVE', '2020-06-24 14:48:27', NULL, 'odao@vae24.ng', '09037996054', NULL, NULL, 'ODAO VAE', NULL, NULL, NULL, 'male', NULL),
+	(18, '502159346846425', 'APP', 'ACTIVE', '2020-06-29 23:07:44', NULL, 'gerrad@merry.com', '', 'gerrad', 'ger203', 'Gerradine Merry', 'staff', 'LOUNGE', NULL, NULL, NULL);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 -- Dumping structure for table fia_db._table
